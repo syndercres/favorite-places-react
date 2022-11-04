25132ef3469e1515f47a2570bf487da474f0e163
syndercombecreswell <syndercombecreswell@gmail.com>
2022-11-04 13:19:52 +0000

    finished website

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -9,6 +9,24 @@ function MainContent(): JSX.Element {
                location={"portland, Weymouth"} 
                description={"a great freestye spot"}
             />
+                        <FavePlace
+               photo={"https://th.bing.com/th/id/R.48c4b563dbda203babb36952ca7c7be8?rik=njpBlxJkhzh%2fAQ&riu=http%3a%2f%2fmedia.gettyimages.com%2fvideos%2fchesil-beach-portland-harbour-weymouth-bay-video-id156031851%3fs%3d640x640&ehk=i9AELpfIboXzGDgVOt4emFiaM%2bfCf0pyasT7gc076ic%3d&risl=&pid=ImgRaw&r=0"}
+               title={"Weymouth Beach"}
+               location={"Weymouth town center"} 
+               description={"good wave spot in the right conditions"}
+            />
+                        <FavePlace
+               photo={"https://th.bing.com/th/id/R.48c4b563dbda203babb36952ca7c7be8?rik=njpBlxJkhzh%2fAQ&riu=http%3a%2f%2fmedia.gettyimages.com%2fvideos%2fchesil-beach-portland-harbour-weymouth-bay-video-id156031851%3fs%3d640x640&ehk=i9AELpfIboXzGDgVOt4emFiaM%2bfCf0pyasT7gc076ic%3d&risl=&pid=ImgRaw&r=0"}
+               title={"Rockley Point"}
+               location={"Hamworthy, Poole"} 
+               description={"Good freeride and freestyle spot with a tricky launch"}
+            />
+                        <FavePlace
+               photo={"https://th.bing.com/th/id/R.48c4b563dbda203babb36952ca7c7be8?rik=njpBlxJkhzh%2fAQ&riu=http%3a%2f%2fmedia.gettyimages.com%2fvideos%2fchesil-beach-portland-harbour-weymouth-bay-video-id156031851%3fs%3d640x640&ehk=i9AELpfIboXzGDgVOt4emFiaM%2bfCf0pyasT7gc076ic%3d&risl=&pid=ImgRaw&r=0"}
+               title={"Sandbanks"}
+               location={"Poole Harbour mouth"} 
+               description={"a great freestye spot"}
+            />
         </main>
     )
     }
