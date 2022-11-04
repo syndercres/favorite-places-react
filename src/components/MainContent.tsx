7b386edb529911dc0d9390d38eb36d295a6245ea
syndercombecreswell <syndercombecreswell@gmail.com>
2022-11-04 14:13:13 +0000

    map

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,14 +1,40 @@
 import FavePlace from "./FavePlace"
+import FavePlaceProps from "./FavePlace"
+
+interface FavePlaceProps {
+    photo: string;
+    title: string;
+    location: string;
+   // map: string;
+    description: string;
+}
+
+
+const FavePlaceArr: FavePlaceProps [] = [
+    {
+        photo: "https://th.bing.com/th/id/R.48c4b563dbda203babb36952ca7c7be8?rik=njpBlxJkhzh%2fAQ&riu=http%3a%2f%2fmedia.gettyimages.com%2fvideos%2fchesil-beach-portland-harbour-weymouth-bay-video-id156031851%3fs%3d640x640&ehk=i9AELpfIboXzGDgVOt4emFiaM%2bfCf0pyasT7gc076ic%3d&risl=&pid=ImgRaw&r=0" ,
+       title: "portland Harbour",
+       location: "portland, Weymouth", 
+       description: "a great freestye spot"
+    }
+]
+
+
+
+
+
+
+
+
+
+
 
 function MainContent(): JSX.Element {
     return(
         <main>
-            <FavePlace
-               photo={"https://th.bing.com/th/id/R.48c4b563dbda203babb36952ca7c7be8?rik=njpBlxJkhzh%2fAQ&riu=http%3a%2f%2fmedia.gettyimages.com%2fvideos%2fchesil-beach-portland-harbour-weymouth-bay-video-id156031851%3fs%3d640x640&ehk=i9AELpfIboXzGDgVOt4emFiaM%2bfCf0pyasT7gc076ic%3d&risl=&pid=ImgRaw&r=0"}
-               title={"portland Harbour"}
-               location={"portland, Weymouth"} 
-               description={"a great freestye spot"}
-            />
+            
+            {FavePlaceArr.map(FavePlace)}
+            
                         <FavePlace
                photo={"https://th.bing.com/th/id/R.48c4b563dbda203babb36952ca7c7be8?rik=njpBlxJkhzh%2fAQ&riu=http%3a%2f%2fmedia.gettyimages.com%2fvideos%2fchesil-beach-portland-harbour-weymouth-bay-video-id156031851%3fs%3d640x640&ehk=i9AELpfIboXzGDgVOt4emFiaM%2bfCf0pyasT7gc076ic%3d&risl=&pid=ImgRaw&r=0"}
                title={"Weymouth Beach"}
